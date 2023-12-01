8c46ba6ef797254852611dd9617465e162e5cd5c
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2023-12-01 14:39:32 +0530

    authapikey in gallary

--- src/collections/GallaryNewResponse.ts
+++ src/collections/GallaryNewResponse.ts
@@ -9,14 +9,8 @@ const GalleryNewResponseAttributes: CollectionConfig = {
     read: () => true,
   },
   auth: {
-    verify: {
-      generateEmailHTML: ({ req, token, user }) => {
-        // Use the token provided to allow your user to verify their account
-        const url = `https://ayathanapayload.payloadcms.app/verify?token=${token}`
-
-        return `Hey ${user.email}, verify your email by clicking here: ${url}`
-      },
-    },
+    useAPIKey: true,
+    disableLocalStrategy: true,
   },
   fields: [
     {
